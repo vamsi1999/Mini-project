c0bf3c779f55a324f3ff6734056c45ac477eede2
vamsi1999 <vamsi.k6669@gmail.com>
2021-07-07 13:45:00 +0530

    code update

--- 3_Implementation/src/Customer_Billing.c
+++ 3_Implementation/src/Customer_Billing.c
@@ -1,3 +1,13 @@
+/******************************************** 
+Project Name:CUSTOMER VIRTUAL WALLET.
+Author: Kadiyala Vamsi
+SF ID :- 314482
+LTTS Step-In Mini Project 
+Source filename: Customer_Billing.c
+Date: 4th-july-2021.
+*********************************************/
+
+
 #include "Customer_Billing.h"
   struct account {
 	char name[100];
@@ -6,39 +16,39 @@
 	char city[100];
 	float balance;
   }customer[1000];
-int cust_count = 0,act_no = 0, id = 1001;
+int cust_count = 0,act_no = 0, id = 10001;
 int i = 0;
 char ch, decision;
 void main(){
     
-	    printf("1: To add account on list\n");
-	    printf("2: To search customer account\n");
-        printf("3: TO pay a bill\n");
-        printf("4: To print info of all customers\n");
-        printf("5: Add balance\n");
-        printf("6: exit\n");
+	    printf("<<<-----------------*-------------------->>>\n1: Add New Customer Wallet.\n");
+	    printf("2: Search Existing Customer Account.\n");
+        printf("3: Pay the Bills.\n");
+        printf("4: Print All Customers Details.\n");
+        printf("5: Top-Up Balance.\n");
+        printf("6: Exit\n<<<-----------------*-------------------->>>\n");
 
-        printf("\nselect what do you want to do?\n");
+        printf("\nselect what do you want to do ?:- ");
 	    
     while(1){
-        scanf("%c", &ch); 
-        switch(ch){
+        scanf("%c",&ch); 
+        switch (ch){
             case '1':
                 cust_count += 1;
-                printf("%d", cust_count);
+                 printf("Serial number:- %d", cust_count);
                 int id = createAccount(i);
                 i += 1;
-                printf("\nYour Account Number is: %d\n\n", id);
+                printf("Your Account Number is:- %d\n\n", id);
                 main();
                 break;
             case '2':
-                printf("Please enter the account number\n");
+                printf("Enter the Account Number:- ");
                 scanf("%d", &act_no);
                 searchACustomer(act_no);
                 main();
                 break;
             case '3':
-                printf("Please enter the account no");
+                printf("Enter the Account Number:- ");
                 scanf("%d", &act_no);
                 payBill(act_no);
                 main();
@@ -48,12 +58,13 @@ void main(){
                 main();
                 break;
             case '5':
-                printf("Please enter account no\n");
+                printf("Enter Your Account Number:- ");
                 scanf("%d", &act_no);
                 addBalance(act_no);
                 main();
                 break;
             case '6':
+                printf("<<<----------Records Saved Successfully<<>>Thanks For Using:):)--------->>>\n\t<<<<<------PROJECT BY VAMSI------->>>>>");
                 exit(0);
                 break;
         }
@@ -63,39 +74,39 @@ void main(){
  }
     int createAccount(int ind){
           customer[ind].acct_no = id++;
-          printf("Enther the name of the customer\n");
+          printf("\nEnter the name of the customer:- ");
           scanf("%s", customer[ind].name);
-          printf("mobile no.\n");
+          printf("Enter Contact Number:-\t");
           scanf("%ld", &customer[ind].mobile_no);
-          printf("city\n");
+          printf("Enter city:- ");
           scanf("%s", customer[ind].city);
-          printf("How much balance you want to maintain\n");
+          printf("How much balance you want to maintain:- ");
           scanf("%f", &customer[ind].balance);
           return customer[ind].acct_no;
       }
       void searchACustomer(int act_no){
           for(int i = 0; i < cust_count; i++){
               if(customer[i].acct_no == act_no){
-                  printf("Customer ID is: %d\n", customer[i].acct_no);
-                  printf("Customer name: %s\n", customer[i].name);
-                  printf("Customer mobile number: %ld\n", customer[i].mobile_no);
-                  printf("customer city name: %s\n", customer[i].city);
-                  printf("Account balance %.2f\n", customer[i].balance);
+                  printf("Customer ID is:- %d\n", customer[i].acct_no);
+                  printf("Customer name:- %s\n", customer[i].name);
+                  printf("Customer mobile number:- %ld\n", customer[i].mobile_no);
+                  printf("customer city name:- %s\n", customer[i].city);
+                  printf("Account balance:- %.2f\n\n", customer[i].balance);
               }
           }
           return;
       }
       void payBill(int act_no){
           float amount = 0;
-          printf("Enter the bill amount: ");
+          printf("Enter the bill amount to Pay:- ");
           scanf("%f", &amount);
           for(int i = 0; i < cust_count; i++){
               if (customer[i].acct_no == act_no){
-                  if (customer[i].balance >= amount){
+                  if (customer[i].balance >= amount) {
                       customer[i].balance -= amount;
-                      printf("Updated Balance is %.2f\n", customer[i].balance);
+                      printf("Updated Balance:- %.2f\n", customer[i].balance);
                   } else {
-                      printf("please add balance\n");
+                      printf("Add Balance to your Wallet:- ");
                       addBalance(act_no);
                   }
               }
@@ -104,24 +115,24 @@ void main(){
       }
       void addBalance(int act_no){
           float amount = 0;
-          printf("Enter the amount you want to add\n");
+          printf("Enter the amount to add for your Wallet:- ");
           scanf("%f", &amount);
           for(int i =0; i < cust_count; i++){
               if (customer[i].acct_no == act_no){
                   customer[i].balance += amount;
-                  printf("Updated account balance: %.2f", customer[i].balance);
+                  printf("Updated account balance:- %.2f\n\n", customer[i].balance);
               }
           }
           return;
       }
-      void printAllCustomers(){
-          for(int i =0; i  <cust_count; i++){
-              printf("CUSTOMER %d\n", (i+1));
-              printf("\t Customer ID: %d\n", customer[i].acct_no);
-              printf("\t customer name: %s\n", customer[i].name);
-              printf("\t customer mobile: %d\n", customer[i].mobile_no);
-              printf("\t Customer city: %s\n", customer[i].city);
-              printf("\t Customer Balance: %.2f\n", customer[i].balance);
+      void printAllCustomers() {
+          for (int i = 0; i < cust_count ; i++ ){
+              printf("CUSTOMER-- %d\n", (i+1));
+              printf("\t Customer ID:- %d\n", customer[i].acct_no);
+              printf("\t customer name:- %s\n", customer[i].name);
+              printf("\t customer mobile:- %ld\n", customer[i].mobile_no);
+              printf("\t Customer city:- %s\n", customer[i].city);
+              printf("\t Customer Balance:- %.2f\n", customer[i].balance);
           }
           return;
       }
